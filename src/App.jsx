4bf06c4d26906ee5f461e05d3807a5e8df279e60
user <user@Users-MacBook-Pro-2.local>
2023-07-24 15:01:57 -0600

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import ButtonAdSub from "./components/ButtonAdSub";
-import { amountFormat } from "./helpers";
+import { amountFormat, calulateTotalToPay } from "./helpers";
 
 function App() {
   //El state va a retornar un arreglo entonces se va extraer de la siguiente manera haciendo destructuring del arreglo
   //El primer valor es el nombre del state, el segundo valor es la funcion que va a modificar el state
   //Dentro del useState a el valor inicial
   const [amount, setAmount] = useState(10000);
+  const [term, setTerm] = useState(6);
+  const [total, setTotal] = useState(0);
+  const [monthlyPayment, setMonthlyPayment] = useState(0);
+
+  //USeEffect siemopre se ejecuta una vez, y luego cada vez cuando el arreglo de dependencia cambien( en esete caso amount y term)
+  //Para usar el total a pagar se usa un useEffect que cambia conforme la cantidad y el plazo cambia
+  //USar la funcion calculateTotalToPay
+  useEffect(() => {
+    const totaltoPay = calulateTotalToPay(amount, term);
+    setTotal(totaltoPay);
+  }, [amount, term]);
+
+  //Pago mensual es dividir total de meses y el total de pago
+  useEffect(() => {
+    setMonthlyPayment(total / term);
+  }, [total]);
 
   const MIN = 0;
   const MAX = 20000;
@@ -61,6 +77,32 @@ function App() {
       <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
         {amountFormat(amount)}
       </p>
+
+      <h2 className="text 2xl font-extrabold text-gray-500  text-center">
+        Elige un <span className="text-indigo-600"> plazo </span> plazo a pagar
+      </h2>
+      <select
+        className="mt-5 w-full p-2 bg-white-border border-gray-300 text-center rounded-lg text-xl font-bold"
+        value={term}
+        onChange={(e) => setTerm(+e.target.value)}
+      >
+        <option value="6"> 6 meses</option>
+        <option value="12"> 12 meses</option>
+        <option value="24"> 24 meses</option>
+      </select>
+      <div className="my-5 space-y-3 bg-gray-50">
+        <h2 className="text 2xl font-extrabold text-gray-500  text-center p-5">
+          Resumen de <span className="text-indigo-600"> Pagos </span>
+        </h2>
+        <p className="text-xl text-center font-bold">{term} Meses</p>
+        <p className="text-xl text-center font-bold">
+          {amountFormat(total)} Total a pagar
+        </p>
+        <p className="text-xl text-center font-bold">
+          Mensualidades por
+          {amountFormat(monthlyPayment)}
+        </p>
+      </div>
     </div>
   );
 }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,4 +6,29 @@ const amountFormat = (amount) => {
   return formatter.format(amount);
 };
 
-export { amountFormat };
+const calulateTotalToPay = (amount, term) => {
+  let total = 0;
+  //mientras mayor es la cantidad solicitada menor es el interes
+  if (amount < 5000) {
+    total = amount * 1.5;
+  } else if (amount >= 5000 && amount < 10000) {
+    total = amount * 1.4;
+  } else if (amount >= 10000 && amount < 15000) {
+    total = amount * 1.3;
+  } else {
+    total = amount * 1.2;
+  }
+
+  //A mayor plazo mayor interes
+  if (term === 6) {
+    //10% mas
+    total *= 1.1;
+  } else if (term === 12) {
+    total *= 1.2;
+  } else {
+    total *= 1.3;
+  }
+  return total;
+};
+
+export { amountFormat, calulateTotalToPay };
